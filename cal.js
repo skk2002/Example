7879b0edf3d15dd4d6d227c3c8c8d4526853b582
skk2002 <129279675+skk2002@users.noreply.github.com>
2023-03-30 15:23:47 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+//console.log(add(1,2));
+ exports.add=add;
+ exports.div=div;
+ exports.mul=mul;
+ exports.sub=sub;
